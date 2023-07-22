79174a1ebf3a45699faec8c1d7113be524cfd9ed
legenie1 <sergeomoffo1@gmail.com>
2023-07-22 17:27:21 +0100

    Code correction

--- src/components/Card/AboutCard.jsx
+++ src/components/Card/AboutCard.jsx
@@ -3,7 +3,7 @@ import React from "react";
 function AboutCard({ url, title, description }) {
   return (
     <div className="text-white animation duration-300 bg-[#1f1f1f] px-12 hover:bg-[#312d58] pt-4 pb-2 rounded-lg flex flex-col gap-12">
-      {/* <div>{url}</div> */}
+      <div>{url}</div>
       <img src={url} alt="" className="w-32 h-32" />
       <div className="flex flex-col gap-3">
         <span className="font-bold text-[18px]">{title}</span>
--- src/pages/about/About.jsx
+++ src/pages/about/About.jsx
@@ -1,53 +1,53 @@
 import React from "react";
-import { FaEye, FaLink, FaRegUserCircle, FaSearch, FaSitemap, FaUserAltSlash } from "react-icons/fa";
+import {
+  FaEye,
+  FaLink,
+  FaRegUserCircle,
+  FaSearch,
+  FaSitemap,
+  FaUserAltSlash,
+} from "react-icons/fa";
 import AboutCard from "../../components/Card/AboutCard";
-import Adobe from '../../assets/Adobe.webp';
-
+import Adobe from "../../assets/Adobe.webp";
 
 function About() {
   const size = 40;
   const abouts = [
     {
       id: 0,
-      url : "https://cdn.pixabay.com/photo/2023/01/23/09/26/cat-7738210__340.jpg",
+      url: Adobe,
       title: "Adobe Suite",
-      description:
-        "85 %",
+      description: "85 %",
     },
     {
       id: 1,
       url: "Adobe",
       title: "Figma​",
-      description:
-        "95 %",
+      description: "95 %",
     },
     {
       id: 3,
-      url: <FaSitemap size={size} />,
+      url: Adobe,
       title: "HTML & CSS​",
-      description:
-        "94 %",
+      description: "94 %",
     },
     {
       id: 4,
-      url: <FaSearch size={size} />,
+      url: Adobe,
       title: "Javascript",
-      description:
-        "65 %",
+      description: "65 %",
     },
     {
       id: 1,
       url: <FaRegUserCircle size={size} />,
       title: "PHP​",
-      description:
-        "70%",
+      description: "70%",
     },
     {
       id: 3,
       url: <FaUserAltSlash size={size} />,
       title: "WordPress & SEM​",
-      description:
-        "94%",
+      description: "94%",
     },
   ];
 
